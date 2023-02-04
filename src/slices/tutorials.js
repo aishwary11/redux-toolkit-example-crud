247ff4596713b78d4f50c514d5bb7de2522b1876
aishwary11 <aishwary46@gmail.com>
2023-02-04 21:33:43 +0530

    some more change

--- src/slices/tutorials.js
+++ src/slices/tutorials.js
@@ -2,48 +2,42 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import TutorialDataService from "../services/tutorial.service";
 
 export const createTutorial = createAsyncThunk(
-  "tutorials/create",
-  async ({ title, description }) => {
+  "tutorials/create", async ({ title, description }) => {
     const res = await TutorialDataService.create({ title, description });
     return res.data;
   }
 );
 
 export const retrieveTutorials = createAsyncThunk(
-  "tutorials/retrieve",
-  async () => {
+  "tutorials/retrieve", async () => {
     const res = await TutorialDataService.getAll();
     return res.data;
   }
 );
 
 export const updateTutorial = createAsyncThunk(
-  "tutorials/update",
-  async ({ id, data }) => {
+  "tutorials/update", async ({ id, data }) => {
     const res = await TutorialDataService.update(id, data);
     return res.data;
   }
 );
 
 export const deleteTutorial = createAsyncThunk(
-  "tutorials/delete",
-  async ({ id }) => {
+  "tutorials/delete", async ({ id }) => {
     await TutorialDataService.delete(id);
     return { id };
   }
 );
 
 export const deleteAllTutorials = createAsyncThunk(
-  "tutorials/deleteAll",
-  async () => {
+  "tutorials/deleteAll", async () => {
     const res = await TutorialDataService.deleteAll();
     return res.data;
   }
 );
 
 export const findTutorialsByTitle = createAsyncThunk(
-  "tutorials/findByTitle",
-  async ({ title }) => {
+  "tutorials/findByTitle", async ({ title }) => {
     const res = await TutorialDataService.findByTitle(title);
     return res.data;
   }
